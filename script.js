6c8efee1c6c8f7d1846ac60150fc355b4df8e319
TheRealAmplifier <coding@jeroenpielage.nl>
2018-06-11 21:53:26 +0200

    added basic code for counting

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 $(function() {
-	alert('test');
+
+	countWords = () => {
+		const regex = /\s/g; // basic regex
+
+		// get textarea input
+		let textInput = document.getElementById('textareaInput').value;
+		
+		// clean up the input and start counting!
+		const totalWordCount = textInput.trim().replace(regex, ' ').split(' ').length;
+		const totalCharacterCount = textInput.length;
+		const totalStrippedCharacterCount = textInput.replace(regex, '').length;
+
+		// target divs & spans
+		const resultDiv = document.querySelector('.results');
+		const wordCountSpan = document.querySelector('.word-count');
+		const characterCountSpan = document.querySelector('.char-count');
+		const characterStrippedCountSpan = document.querySelector('.char-stripped-count');
+
+		// check if there is any text inside the textarea
+		if( textInput.length >= 1) {
+			// show results div.
+			resultDiv.style.display = 'block';
+
+			// populate result spans
+			wordCountSpan.textContent = totalWordCount;
+			characterCountSpan.textContent = totalCharacterCount;
+			characterStrippedCountSpan.textContent = totalStrippedCharacterCount;
+		}
+	}
+
+	document.getElementById('countWords').addEventListener('click', function(event) {
+		event.preventDefault();
+
+		countWords();
+	});
+
 });
